b00da6050a968d9438ffb394afb0939947fca4aa
Brian <Brian@Smilodon>
2011-03-29 17:08:36 -0700

    ADDED: Added project to repo

---
+++ MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TouchScreenComicViewer {
+	public partial class MainPage : UserControl {
+		private const double _originalWidth = 970;
+		private const double _originalHeight = 1470;
+		private const double _originalAspectRatio = 
+				_originalWidth / _originalHeight;
+		public MainPage() {
+
+			InitializeComponent();
+
+			SizeChanged += new SizeChangedEventHandler(MainPage_SizeChanged);
+		}
+
+		void MainPage_SizeChanged(object sender, SizeChangedEventArgs e) {
+			if (e.NewSize.Width < _originalWidth ||
+					e.NewSize.Height < _originalHeight) {
+				// don't shrink
+				PageScale.ScaleX = 1.0;
+				PageScale.ScaleY = 1.0;
+			} else {
+				// resize keeping aspect ratio the same
+				if (e.NewSize.Width / e.NewSize.Height > _originalAspectRatio) {
+					// height is our constraining property
+					PageScale.ScaleY = e.NewSize.Height / _originalHeight;
+					PageScale.ScaleX = PageScale.ScaleY;
+				} else {
+					// either width is our constraining property, or the user
+					// managed to nail our aspect ratio perfectly.
+					PageScale.ScaleX = e.NewSize.Width / _originalWidth;
+					PageScale.ScaleY = PageScale.ScaleX;
+				}
+			}
+		}
+	}
+}
