d7b3dc08546f0eeb2665f761a58ea28295623d03
Brian <Brian@Smilodon>
2011-06-09 20:31:00 -0700

    ADDED: Added comic archive manager class to manage the comic book archive data

---
+++ ComicArchiveManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TouchScreenComicViewer {
+	public class ComicArchiveManager {
+		const string COMIC_ARCHIVE_ZIP_EXT = ".cbz";
+
+		public ComicArchiveManager() {
+		}
+
+		public List<string> GetAvailableComics() {
+			return IsoStorageUtilities.GetIsolatedStorageFilesWithExtension(COMIC_ARCHIVE_ZIP_EXT);
+		}
+
+	}
+
+}
