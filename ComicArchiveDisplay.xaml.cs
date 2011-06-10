ad3c6b3d2dbf71a05ee9817df5aff7571b326b0c
Brian <Brian@Smilodon>
2011-06-09 22:16:20 -0700

    CHANGED: Removed unused function

--- ComicArchiveDisplay.xaml.cs
+++ ComicArchiveDisplay.xaml.cs
@@ -32,20 +32,5 @@ namespace TouchScreenComicViewer {
 			((UserControlContainer)Application.Current.RootVisual).SwitchControl(comicDisplay);
 		}
 
-		private childItem FindVisualChild<childItem>(DependencyObject obj)
-		where childItem : DependencyObject {
-			for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++) {
-				DependencyObject child = VisualTreeHelper.GetChild(obj, i);
-				if (child != null && child is childItem)
-					return (childItem)child;
-				else {
-					childItem childOfChild = FindVisualChild<childItem>(child);
-					if (childOfChild != null)
-						return childOfChild;
-				}
-			}
-			return null;
-		}
-
 	}
 }
